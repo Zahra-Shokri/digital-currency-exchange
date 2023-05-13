c53a340447dad73c49013028f599cbf23f23d28c
Zahra Shokri <zvahedi5@gmail.com>
2023-05-13 16:56:34 +0330

    Create Coin Component with CSS

---
+++ src/components/Coin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styles
+import styles from '../styles/Coin.module.css';
+
+
+const Coin = ({ symbol, name, image, currentPrice, marketCap, priceChangePercentage}) => {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Coin;
