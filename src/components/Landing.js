e6c5e19e946596e34cea7cb8cc71456f8db6dd2c
Zahra Shokri <zvahedi5@gmail.com>
2023-05-13 16:27:33 +0330

    Get API

---
+++ src/components/Landing.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+// Functions
+import { getAPI } from '../services/api';
+
+const Landing = () => {
+    const [coins, setCoins] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async() => {
+            const data = await getAPI();
+            setCoins(data);
+            console.log(data);
+        }
+
+        fetchAPI();
+    }, [])
+
+    return(
+        <>
+        
+        </>
+    );
+}
+
+export default Landing;
