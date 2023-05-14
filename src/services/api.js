b99282f0c3b2c8be9fcd4205f060ec6e825cdb20
Zahra Shokri <zvahedi5@gmail.com>
2023-05-14 13:38:35 +0330

    Set comment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-
+// Variables
 const BASE_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false';
+
 
 const getAPI = async () => {
     const response = await axios.get(BASE_URL);
